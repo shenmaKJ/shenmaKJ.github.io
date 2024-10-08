2fec3230341108bb72e35f49d81c54625429a91a
shenmaKJ <1005330519@qq.com>
2024-10-09 02:06:10 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// 使用 cURL 获取目标页面内容并显示，保持当前域名
+$url = "http://www.huarui888.live/";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, false);
+
+$response = curl_exec($ch);
+
+if (curl_errno($ch)) {
+    echo "请求错误: " . curl_error($ch);
+} else {
+    echo $response;
+}
+
+curl_close($ch);
+?>
